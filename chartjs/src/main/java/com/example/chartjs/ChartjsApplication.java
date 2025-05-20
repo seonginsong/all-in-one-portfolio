b5e85aedd92640c7f90933004f73753eb0ea68ed
GD <as45655@gmail.com>
2025-05-20 17:21:56 +0900

    1

--- chartjs/src/main/java/com/example/chartjs/ChartjsApplication.java
+++ chartjs/src/main/java/com/example/chartjs/ChartjsApplication.java
@@ -2,7 +2,9 @@ package com.example.chartjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ChartjsApplication {
 
